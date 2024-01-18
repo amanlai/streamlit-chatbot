f954dcf9515ca146b22d30716e2ea011eda73c1e
amanlai <manlai.lp@gmail.com>
2024-01-18 08:44:13 -0800

    added 'solution' to sqlite version issue on streamlit

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,8 @@
+# for SQLite versioning issue on Streamlit Cloud
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import os
 from datetime import datetime
 from tempfile import NamedTemporaryFile
